6b7df961e31b8eb8049aa82e43f54b99a966278e
sminlee123 <tmdals3622@naver.com>
2025-08-25 17:05:32 +0900

    refactor: 빌드 오류

--- src/test/java/com/example/movieplatform/MoviePlatformApplicationTests.java
+++ src/test/java/com/example/movieplatform/MoviePlatformApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.movieplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MoviePlatformApplicationTests {
     @Test
     void contextLoads() {
