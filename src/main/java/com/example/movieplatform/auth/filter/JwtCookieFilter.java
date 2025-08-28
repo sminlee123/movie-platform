9d721d79c2cb53f3acc7747ce0948cd04500ffe9
sminlee123 <tmdals3622@naver.com>
2025-08-28 16:33:57 +0900

    feat: 인증정보 컨텍스트에 저장

--- src/main/java/com/example/movieplatform/auth/filter/JwtCookieFilter.java
+++ src/main/java/com/example/movieplatform/auth/filter/JwtCookieFilter.java
@@ -1,54 +1,82 @@
-package com.example.movieplatform.auth.filter;
+    package com.example.movieplatform.auth.filter;
 
-import com.example.movieplatform.auth.utils.JwtUtil;
-import com.example.movieplatform.user.service.UserService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.filter.OncePerRequestFilter;
+    import com.example.movieplatform.auth.utils.JwtUtil;
+    import com.example.movieplatform.user.service.UserService;
+    import jakarta.servlet.FilterChain;
+    import jakarta.servlet.ServletException;
+    import jakarta.servlet.http.Cookie;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+    import org.springframework.security.core.GrantedAuthority;
+    import org.springframework.security.core.authority.SimpleGrantedAuthority;
+    import org.springframework.security.core.context.SecurityContextHolder;
+    import org.springframework.stereotype.Component;
+    import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+    import java.io.IOException;
+    import java.util.List;
 
-@RequiredArgsConstructor
-public class JwtCookieFilter extends OncePerRequestFilter {
+    @Component
+    @RequiredArgsConstructor
+    public class JwtCookieFilter extends OncePerRequestFilter {
 
-    private final JwtUtil jwtUtil;
-    private final UserService userService;
+        private final JwtUtil jwtUtil;
+        private final UserService userService;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        FilterChain filterChain) throws ServletException, IOException {
 
-        String accessToken = getTokenFromCookies(request, "ACCESSTOKEN");
-        String refreshToken = getTokenFromCookies(request, "REFRESHTOKEN");
+            String accessToken = getTokenFromCookies(request, "ACCESSTOKEN");
+            String refreshToken = getTokenFromCookies(request, "REFRESHTOKEN");
 
-        if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+            if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+                authenticateUser(accessToken);
+            } else if (refreshToken != null && jwtUtil.validateToken(refreshToken)) {
+                String userEmail = jwtUtil.getUserEmail(refreshToken);
+                String role = userService.getUserRole(userEmail);
 
-        } else if (refreshToken != null && jwtUtil.validateToken(refreshToken)) {
-            String userEmail = jwtUtil.getUserEmail(refreshToken);
-            String role = userService.getUserRole(userEmail);
+                // 리프레시 토큰으로 엑세스 토큰 재발급
+                String newToken = jwtUtil.generateAccessToken(userEmail, role);
 
-            // 리프레시 토큰으로 엑세스 토큰 재발급
-            String newToken = jwtUtil.generateAccessToken(userEmail, role);
+                Cookie cookie = new Cookie("ACCESSTOKEN", newToken);
+                response.addCookie(cookie);
 
-            Cookie cookie = new Cookie("ACCESSTOKEN", newToken);
-            response.addCookie(cookie);
+                authenticateUser(newToken);
+            } else  {
+                // 인증 실패 시 로그인 폼으로 리다이렉트
+                response.sendRedirect("/auth/loginForm");
+                return;
+            }
+            filterChain.doFilter(request, response);
         }
-    }
 
-    // 쿠키 추출 메서드
-    private String getTokenFromCookies(HttpServletRequest request, String name) {
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if (cookie.getName().equals(name)) {
-                    return cookie.getValue();
+        // 쿠키 추출 메서드
+        private String getTokenFromCookies(HttpServletRequest request, String name) {
+            if (request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equals(name)) {
+                        return cookie.getValue();
+                    }
                 }
             }
+            return null;
         }
-        return null;
+
+        // 인증 정보를 컨텍스트 홀더에 저장
+        private void authenticateUser(String token) {
+            String userEmail = jwtUtil.getUserEmail(token);
+            String role = jwtUtil.getRole(token);
+
+            // 인증 객체 생성자에 들어갈 권한 컬렉션
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userEmail, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
     }
-}
--- src/main/java/com/example/movieplatform/common/config/SecurityConfig.java
+++ src/main/java/com/example/movieplatform/common/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.example.movieplatform.common.config;
 
+import com.example.movieplatform.auth.filter.JwtCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtCookieFilter jwtCookieFilter) throws Exception {
         http
                 .securityMatcher("/**") // 모든 요청
                 .authorizeHttpRequests(authorize -> authorize
@@ -17,7 +20,8 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**", "/users").permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable()); // 람다 방식으로 CSRF 비활성화
+                .csrf(csrf -> csrf.disable()) // 람다 방식으로 CSRF 비활성화
+                .addFilterBefore(jwtCookieFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
