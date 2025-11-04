6c144b8faa52def5285f8c20e542d88238b9e525
sminlee123 <tmdals3622@naver.com>
2025-11-04 15:39:19 +0900

    feat: jwt 헤더 추가

--- src/main/java/com/example/movieplatform/auth/controller/RefreshTokenController.java
+++ src/main/java/com/example/movieplatform/auth/controller/RefreshTokenController.java
@@ -32,22 +32,22 @@ public class RefreshTokenController {
             @CookieValue(value = "REFRESHTOKEN", required = false) String refreshToken,
             HttpServletResponse response
     ) {
+        log.info("POST /api/auth/refresh started.");
+
         if (refreshToken == null) {
-            log.warn("Refresh token cookie not Exist.");
+            log.warn("Refresh token not found");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh token is missing.");
         }
 
         String userEmail;
 
         try {
-           jwtUtil.validateToken(refreshToken);
-           userEmail = jwtUtil.getUserEmail(refreshToken);
+            jwtUtil.validateToken(refreshToken);
+            userEmail = jwtUtil.getUserEmail(refreshToken);
         } catch (ExpiredJwtException e) {
-            log.warn("Expired refresh token cookie.");
             clearRefreshTokenCookie(response);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Expired refresh token.");
         } catch (Exception e) {
-            log.warn("Invalid refresh token.", e);
             clearRefreshTokenCookie(response);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token.");
         }
@@ -56,12 +56,15 @@ public class RefreshTokenController {
         String newAccessToken = jwtUtil.generateAccessToken(userEmail, role);
 
         String newRefreshToken = jwtUtil.generateRefreshToken(userEmail);
-        addRefreshToken(response, newRefreshToken);
-        
+        addRefreshToken(response, newRefreshToken); // 새 리프레시 토큰 추가
+
+        log.info("AccessToken and RefreshToken Issue {}.", userEmail);
         return ResponseEntity.ok(Map.of("accessToken", newAccessToken));
     }
-    
+
     private void addRefreshToken(HttpServletResponse response, String newRefreshToken) {
+        log.info("Adding RefreshToken");
+
         Cookie refreshToken = new Cookie("REFRESHTOKEN", newRefreshToken);
         refreshToken.setHttpOnly(true);
         refreshToken.setPath("/");
@@ -72,6 +75,8 @@ public class RefreshTokenController {
     }
 
     private void clearRefreshTokenCookie(HttpServletResponse response) {
+        log.warn("Clearing RefreshToken");
+
         Cookie cookie = new Cookie("REFRESHTOKEN", null);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
